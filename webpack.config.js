c2ad11eb01c98d9b67f9ef42bc809d0315e522cb
Shinhyojeong <hyojeonge0@naver.com>
2022-03-24 15:20:18 +0900

    🔧 config : babel 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true
   },
   resolve: {
-    extensions: ['.js']
+    extensions: [".js"]
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"]
       }
     ]
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: "./index.html"
     }),
     new MiniCssExtractPlugin()
   ],
   devServer: {
     open: true
   }
-};
+}
