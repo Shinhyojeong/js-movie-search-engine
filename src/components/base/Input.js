da807da3527a40a34dd645f1dad5e06bad7321ef
Shinhyojeong <hyojeonge0@naver.com>
2022-03-24 16:50:04 +0900

    ✨ feat : Input 컴포넌트 구현

---
+++ src/components/base/Input.js
@@ -0,0 +1,28 @@
+import { createElement, applyClassName } from "@utils/handleElement"
+
+export default function Input({ targetEl, initialState, onChange }) {
+  const inputEl = createElement({ elType: "input" })
+
+  this.state = initialState
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    this.render()
+  }
+
+  this.render = () => {
+    const { elClassName, placeholder, value } = this.state
+
+    applyClassName(inputEl, elClassName)
+
+    inputEl.placeholder = placeholder ?? ""
+    inputEl.value = value ?? ""
+  }
+
+  this.render()
+  targetEl.append(inputEl)
+
+  inputEl.addEventListener("input", (e) => {
+    onChange(e)
+  })
+}
